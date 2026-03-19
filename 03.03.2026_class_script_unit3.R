20a01ad43caf71662917d8225de87569eb38c0aa
juliamnuss <jnuss@seoe.sc.edu>
2026-03-19 09:52:08 -0400

    sfdg

--- 03.03.2026_class_script_unit3.R
+++ 03.03.2026_class_script_unit3.R
@@ -164,3 +164,126 @@ ggplot() +
   geom_line(data = lm_5_predict, aes (y = .fitted, x = bill_length_mm, color = species), linewidth = 1) +
   theme_bw() + 
   labs( x = "Bill Length (mm)", y = "Bill Depth (mm)", title = "Predictions #3")
+
+##########################################################################################
+# 03.17.2026
+# JMN
+# Class Script
+# building an interaction term
+
+lm_6 = lm(bill_depth_mm ~ bill_length_mm + species + bill_length_mm * species, data = penguins_lm5)
+# shorthand of this => lm_6 = lm(bill_depth_mm ~ bill_length_mm*species, data = penguins_lm5)
+# if you wanted just the interaction terms => lm_6 = lm(bill_depth_mm ~ bill_length_mm : species, data = penguins_lm5) # but this is strange to do
+summary(lm_6)
+
+# one way to compare two models
+AIC(lm_5, lm_6)
+AIC(lm_5)
+# what matters is the difference in AIC between two models that use the same data
+# rule of thumb is that if the difference in AIC is greater than a magnitude of 2 AIC units, then the smaller AIC value is the better fit
+# AIC is a calculation of the liklihood of the model being accurate and is penalized against the complexity of the model
+# can also compare adjusted R squared
+
+# step function
+# use this on your most complex model
+step(lm_6)
+# takes complex model and removes terms one by one to create simplest, best-fit model. 
+# will show you the difference in the AICs for the modified model and will stop at the best model configuration
+
+lm_6_predict = lm_6 %>% 
+  broom::augment(se_fit = T, interval = "confidence")
+head(lm_6_predict)
+
+ggplot(data = lm_6_predict) +
+  geom_point(aes(x = bill_length_mm, y = bill_depth_mm, col = species)) +
+  theme_bw() + 
+  labs( x = "Bill Length (mm)", y = "Bill Depth (mm)", title = "penguins") + 
+  geom_line(aes(x = bill_length_mm, y = .fitted, color = species)) + 
+  geom_ribbon(aes(ymin = .lower, ymax = .upper, x = bill_length_mm, fill = species), alpha = 0.3)
+
+# Multiple regression with more than one continuous variable predictor
+library(car) # vif()
+library(ggiraph)
+library(ggiraphExtra) # ggPredict()
+
+gentoo = penguins %>% 
+  filter(species=="Gentoo")
+
+# build simple linear regression
+lm_gentoo_1 = lm(bill_depth_mm ~ bill_length_mm, data = gentoo)
+
+# build multiregression with 2 variables
+lm_gentoo_2 = lm(bill_depth_mm ~ bill_length_mm + flipper_length_mm, data = gentoo)
+
+# build multiregression with 3 variables
+lm_gentoo_3 = lm(bill_depth_mm ~ bill_length_mm + flipper_length_mm + body_mass_g, data = gentoo)
+
+AIC(lm_gentoo_1, lm_gentoo_2, lm_gentoo_3) # lm_gentoo_3 performs the best
+best_model = step(lm_gentoo_3)
+summary(best_model)
+
+vif(lm_gentoo_3) # vif values ~2, mild multicollinearity
+
+### look at bill depth ~ body mass while holding bill length and flipper length constant
+# use expand to get full range of 1 variable, then add in median of other varibale(s) from original data
+newdata = gentoo %>%
+  select(body_mass_g) %>%
+  mutate(bill_length_mm = median(gentoo$bill_length_mm, na.rm = TRUE), 
+        flipper_length_mm = median(gentoo$flipper_length_mm, na.rm = TRUE))
+
+lm_gentoo_3_predict = lm_gentoo_3 %>%
+  broom::augment(newdata = newdata, se_fit = TRUE, interval = "confidence") # ?augment.lm
+
+# plot the data and model predictions
+ggplot(data = lm_gentoo_3_predict) +
+  geom_point(aes(x = body_mass_g, y = bill_depth_mm), data = gentoo) + 
+  geom_ribbon(aes(ymin = .lower, ymax = .upper, x = body_mass_g), alpha = 0.15) +
+  geom_line(aes(y = .fitted, x = body_mass_g)) + 
+  annotate("text", x = 4250, y=17, label = paste0("flipper length = ", 
+            median(gentoo$flipper_length_mm, na.rm = TRUE), " mm")) +
+  annotate("text", x = 4250, y = 16.5, label = paste0("bill length = ", 
+            median(gentoo$bill_length_mm, na.rm = TRUE), " mm")) +
+  theme_bw()
+
+# Exercise 5.3 -> did not work for me :(
+newdata_2 = gentoo %>%
+  select(flipper_length_mm) %>%
+  mutate(bill_length_mm = median(gentoo$body_mass_g, na.rm = TRUE), 
+        body_mass_g = median(gentoo$bill_length_mm, na.rm = TRUE))
+
+lm_gentoo_3_predict_2 = lm_gentoo_3 %>%
+  broom::augment(newdata = newdata_2, se_fit = TRUE, interval = "confidence") # ?augment.lm
+
+# plot the data and model predictions
+ggplot(data = lm_gentoo_3_predict_2) +
+  geom_point(aes(x = flipper_length_mm, y = bill_depth_mm), data = gentoo) + 
+  geom_ribbon(aes(ymin = .lower, ymax = .upper, x = flipper_length_mm), alpha = 0.15) +
+  geom_line(aes(y = .fitted, x = flipper_length_mm)) + 
+  annotate("text", x = 210, y=17, label = paste0("body mass = ", 
+            median(gentoo$body_mass_mm, na.rm = TRUE), " mm")) +
+  annotate("text", x = 210, y = 16.5, label = paste0("bill length = ", 
+            median(gentoo$bill_length_mm, na.rm = TRUE), " mm")) +
+  theme_bw()
+
+### ANOVA
+head(penguins)
+#conduct an ANOVA using lm()
+penguin_lm = lm(body_mass_g ~ species + sex, data = penguins)
+summary (penguin_lm)
+anova(penguin_lm)
+
+# conduct the same ANOVS using aov()
+penguin_anova = aov(body_mass_g ~ species + sex, data = penguins)
+summary(penguin_anova)
+
+# which sex has higher body mass?
+penguins %>%
+  group_by(sex) %>%
+  summarize(mean_body_mass_g = mean(body_mass_g))
+
+# which species has higher body mass?
+penguins %>% 
+  group_by(species) %>% 
+  summarize(mean_body_mass_g = mean(body_mass_g, na.rm = TRUE))
+
+TukeyHSD(penguin_anova) # reguires the output of the aov() function
